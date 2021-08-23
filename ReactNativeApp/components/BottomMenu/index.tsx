ed3a6b5c4433734a3de99f45fada5a3c1d7eaf0f
Amandine Eap <amandine@afterdigital.co.uk>
2021-08-23 21:34:04 +0100

    :sparkles: Style Bottom Nav

--- ReactNativeApp/components/BottomMenu/index.tsx
+++ ReactNativeApp/components/BottomMenu/index.tsx
@@ -1,21 +1,58 @@
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import Exclusive from '../Exclusive'
-import Tourdates from '../Tourdates'
-import Shop from '../Shop'
-import Settings from '../Settings'
+import Exclusive from '../Exclusive';
+import Tourdates from '../Tourdates';
+import Shop from '../Shop';
+import Settings from '../Settings';
 
 const Tab = createBottomTabNavigator();
 
-const BottomMenu = ()=> {
+const TabScreenStyles = {
+  tabBarActiveBackgroundColor: 'white',
+  tabBarActiveTintColor: 'black',
+  tabBarInactiveBackgroundColor: 'black',
+  tabBarInactiveTintColor: 'white',
+};
+
+const BottomMenu = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Exclusive" component={Exclusive} />
-      <Tab.Screen name="Tourdates" component={Tourdates} />
-      <Tab.Screen name="Shop" component={Shop} />
-      <Tab.Screen name="Settings" component={Settings} />
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Exclusive') {
+            iconName = 'excluIcon';
+          } else if (route.name === 'Tourdates') {
+            iconName = 'tourIcon';
+          } else if (route.name === 'Shop') {
+            iconName = 'shopIcon';
+          } else if (route.name === 'Settings') {
+            iconName = 'settingsIcon';
+          }
+
+          return <Text style={{color: focused? 'black': 'white'}}>{iconName}</Text>;
+        },
+      })}>
+      <Tab.Screen
+        name="Exclusive"
+        component={Exclusive}
+        options={TabScreenStyles}
+      />
+      <Tab.Screen
+        name="Tourdates"
+        component={Tourdates}
+        options={TabScreenStyles}
+      />
+      <Tab.Screen name="Shop" component={Shop} options={TabScreenStyles} />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={TabScreenStyles}
+      />
     </Tab.Navigator>
   );
-}
-export default BottomMenu
+};
+export default BottomMenu;
