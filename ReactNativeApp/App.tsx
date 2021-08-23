85161f917520c36490f58909ec42c0eaba59f4d0
Amandine Eap <amandine@afterdigital.co.uk>
2021-08-23 21:31:56 +0100

    :fire: Remove unused imports
    Remove unused imports

--- ReactNativeApp/App.tsx
+++ ReactNativeApp/App.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
-import { View, Text,Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import Routes from './components/Routes';
-import BottomMenu from './components/BottomMenu'
+import {NavigationContainer} from '@react-navigation/native';
+import BottomMenu from './components/BottomMenu';
 
-const App = () =>  {
+const App = () => {
   return (
     <NavigationContainer>
-      {/* <Routes/> */}
-      <BottomMenu/>
+      <BottomMenu />
     </NavigationContainer>
   );
-}
+};
 
 export default App;
