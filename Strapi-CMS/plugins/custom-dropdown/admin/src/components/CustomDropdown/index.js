8270e869b7c87ab5d39429797acf2bd1e1e43c83
Amandine Eap <amandine@afterdigital.co.uk>
2021-09-10 15:53:56 +0100

    :sparkles: Fetch third party data and save from custom plugin strapi

--- Strapi-CMS/plugins/custom-dropdown/admin/src/components/CustomDropdown/index.js
+++ Strapi-CMS/plugins/custom-dropdown/admin/src/components/CustomDropdown/index.js
@@ -1,13 +1,24 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import Select from "react-select";
 
-const CustomDropdown = () => {
+const CustomDropdown = (props) => {
+  console.log("props"), props;
+  const [selected, setSelected] = useState(
+    props.value ? JSON.parse(props.value) : null
+  );
+
   const handleChange = (newValue, actionMeta) => {
     console.group("Value Changed");
     console.log(newValue);
     console.log(`action: ${actionMeta.action}`);
     console.groupEnd();
+    const save = JSON.stringify(newValue);
+    setSelected(newValue);
+    props.onChange({
+      target: { name: "customDropdown", value: save },
+    });
   };
   const handleInputChange = (inputValue, actionMeta) => {
     console.group("Input Changed");
@@ -15,15 +26,29 @@ const CustomDropdown = () => {
     console.log(`action: ${actionMeta.action}`);
     console.groupEnd();
   };
+
+  const [opts, setOpts] = useState([]);
+  useEffect(() => {
+    const test = async () => {
+      const { data } = await axios.get(
+        "https://api.doge-meme.lol/v1/memes/?skip=0&limit=100"
+      );
+      const { data: options } = data;
+      const all = [];
+      options.map((d) =>
+        all.push({ value: d.submission_id, label: d.submission_title })
+      );
+      setOpts(all);
+    };
+    test();
+  }, []);
   return (
     <Select
       isClearable
       onChange={handleChange}
       onInputChange={handleInputChange}
-      options={[
-        { label: "test", value: "1" },
-        { label: "woo", value: 2 },
-      ]}
+      options={opts}
+      defaultValue={selected}
     />
   );
 };
