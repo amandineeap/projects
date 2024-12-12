e23db6d8057830d7ab2786d0ff5ad89b22435126
Amandine Eap <amandine.eap@gmail.com>
2024-12-12 15:27:27 +0000

    Fix test ?!

--- rails-7-agile-book/depot/app/controllers/orders_controller.rb
+++ rails-7-agile-book/depot/app/controllers/orders_controller.rb
@@ -29,13 +29,16 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
-        Cart.destroy[session[:cart_id]]
+        Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        format.html { redirect_to store_index_url, notice: "Thank you for your order." }
-        format.json { render :show, status: :created, location: @order }
+        format.html { redirect_to store_index_url, notice: 
+          'Thank you for your order.' }
+        format.json { render :show, status: :created,
+          location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.json { render json: @order.errors,
+          status: :unprocessable_entity }
       end
     end
   end
--- rails-7-agile-book/depot/test/controllers/orders_controller_test.rb
+++ rails-7-agile-book/depot/test/controllers/orders_controller_test.rb
@@ -31,15 +31,15 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should create order" do
-  #   assert_difference("Order.count") do
-  #     post orders_url, params: { order: { address: @order.address,
-  #       email: @order.email, name: @order.name,
-  #       pay_type: @order.pay_type } }
-  #   end
+  test "should create order" do
+    assert_difference("Order.count") do
+      post orders_url, params: { order: { address: @order.address,
+        email: @order.email, name: @order.name,
+        pay_type: @order.pay_type } }
+    end
 
-  #   assert_redirected_to store_index_url
-  # end
+    assert_redirected_to store_index_url
+  end
 
   test "should show order" do
     get order_url(@order)
