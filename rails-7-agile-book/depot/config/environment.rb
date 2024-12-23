434aba2792e1f3df7d7c810a97fd43711b6d82e1
Amandine Eap <amandine.eap@gmail.com>
2024-12-23 15:25:22 +0000

    Add Emails and test them

--- rails-7-agile-book/depot/config/environment.rb
+++ rails-7-agile-book/depot/config/environment.rb
@@ -11,3 +11,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+end
