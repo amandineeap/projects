c56cc32df7fb6ad3ac5e052ed14a5405a5c56e8f
Amandine Eap <amandine.eap@gmail.com>
2024-12-30 16:17:36 +0000

    Allow rich text response to support requests

--- rails-7-agile-book/depot/config/routes.rb
+++ rails-7-agile-book/depot/config/routes.rb
@@ -16,8 +16,14 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
 
+
+  # START_HIGLIGHT
+  resources :support_requests, only: %i[ index update ]
+  # END_HIGLIGHT
   resources :users
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   scope '(:locale)' do
     resources :orders
@@ -25,8 +31,4 @@ Rails.application.routes.draw do
     resources :carts
     root 'store#index', as: 'store_index', via: :all
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
